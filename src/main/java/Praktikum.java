9dcf5442c442b9346041ae21ea38a89d1405db85
Marin <marin@bifit.com>
2022-08-02 20:43:12 +0300

    finished task

---
+++ src/main/java/Praktikum.java
@@ -0,0 +1,22 @@
+
+public class Praktikum {
+    /**
+     * В этом методе заложи логику работы с классом Account.
+     * Нужно создать экземпляр класса Account: в качестве аргумента передать тестируемое имя
+     * и вызвать метод, который проверяет, можно ли использовать фамилию и имя для печати на банковской карте.
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        final String name = " NAMENAME";
+
+        Account account = new Account(name);
+
+        if (account.checkNameToEmboss()) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+    }
+}
